6f5cc24ae5689ab45bb54017fcc89b9937190c77
sandeep <sandeepqtpexper@gmail.com>
2019-07-28 15:03:19 +0530

    updated

--- Flipkart/src/main/java/LoginPage.java
+++ Flipkart/src/main/java/LoginPage.java
@@ -6,29 +6,78 @@ import org.testng.annotations.Test;
 
 public class LoginPage {
 	//1--- Search Vowels pos using Pattern
-	@Test(enabled=false)
-	public void searchVowelsUsingPattern() {
-		String vwpos=null;
+	@Test
+	public void asearchVowelsUsingPattern() {
+		String vwpos="";
 		String x="sandeep kumar yadav";
 		Pattern pattern = Pattern.compile("[aeiou]", Pattern.CASE_INSENSITIVE); 
 		Matcher m=pattern.matcher(x);
 		while (m.find()) {
 			vwpos =vwpos+" "+String.valueOf(m.start());
-			
+
 		}
-		System.out.println(vwpos);
+		System.out.println("Vowels pos ==="+vwpos);
+		System.out.println("------------------------------------");
+		System.out.println("------------------------------------");
 	}
-	
-	//1--- Search Vowels pos using Pattern
-		@Test/*(enabled=false)*/
-		public void pattern1() {
-		for(int i=1;i<=4;i++) {
-			
+
+	//2---Creating Pattern
+	@Test
+	public void pattern1() {
+
+		for(int i=1;i<=10;i++) {
+			String x="";
 			for(int j=1;j<=i;j++ ) {
-				
+				x=x+"*";
 			}
-			
+			System.out.println(x);
+
 		}
+		System.out.println("------------------------------------");
+		System.out.println("------------------------------------");
+	}
+	///********************************************
+
+	//3---Creating Pattern
+	@Test 
+	public void pattern3() {
+		for(int i=10;i>=1;i--) {
+			String x="";
+			for(int j=i;j>=1;j-- ) {
+				x=x+"*";
+			}
+			System.out.println(x);
+
 		}
-	
+		System.out.println("------------------------------------");
+		System.out.println("------------------------------------");
+	}
+	///********************************************
+
+	//4---Creating Pattern
+	@Test
+	public void pattern4() {
+		for(int i=1;i<=10;i++) {
+			String x="";
+			for(int j=1;j<=i;j++ ) {
+				x=x+"*";
+			}
+			System.out.println(x);
+		}		
+		for(int i=9;i>=1;i--) {
+			String x="";
+			for(int j=i;j>=1;j-- ) {
+				x=x+"*";
+			}
+			System.out.println(x);
+
+		}
+		System.out.println("------------------------------------");
+		System.out.println("------------------------------------");
+	}
+	///********************************************		
+
+
+
+
 }
